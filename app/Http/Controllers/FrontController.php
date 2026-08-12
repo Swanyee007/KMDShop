9b0fc518ffda67520eab489733a34d0767d4ba75
Swan Yee <swanyeekhine@gmail.com>
2026-08-12 11:29:58 +0630

    fourth

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -10,4 +10,9 @@ class FrontController extends Controller
     {
         return view('front.shop');
     }
+    public function shopItem($id)
+
+    {
+        return view ('front.shop-item');
+    }
 }

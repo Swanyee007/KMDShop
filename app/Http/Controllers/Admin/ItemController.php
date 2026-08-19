e39ca80896b10b04866cf20e0a430830ca99b8f9
Swan Yee <swanyeekhine@gmail.com>
2026-08-19 11:29:46 +0630

    eighth

--- app/Http/Controllers/Admin/ItemController.php
+++ app/Http/Controllers/Admin/ItemController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Item;
 use App\Models\Category;
+use App\Http\Requests\ItemRequest;
 class ItemController extends Controller
 {
     /**
@@ -30,7 +31,7 @@ class ItemController extends Controller
      * Store a newly created resource in storage.
      */
 
-    public function store(Request $request)
+    public function store(ItemRequest $request)
     {
         //dd($request);
         $items=Item::create($request->all());
